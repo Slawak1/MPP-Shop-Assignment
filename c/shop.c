b4a938d5ab1e26e7e512579689dde8084a52c93c
slawak1 <sowa.slawomir@gmail.com>
2020-12-10 16:58:23 +0000

    Shop.c is done!!

--- c/shop.c
+++ c/shop.c
@@ -1,7 +1,20 @@
+/*
+Student: Slawomir Sowa
+Date: 10/12/2020
+Student ID: G000XXXXX
+MultiParadigmProgramming Assignmetn
+Title: Shop
+Laguage: C 
+
+
+
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+
 
 struct Product {
 	// datatype struct contain product name and price
@@ -32,33 +45,30 @@ struct Customer {
 
 
 
-//####################################################################//
-
-
-
 void printProduct(struct Product p)
 /* 
-	Method to print information about product
+	Function to print information about product
 	Parameters:
-		p : struct : Holds information about product name and price
+		p : struct Product : Holds information about product name and price
 */
 {
-	printf("PRODUCT NAME: %s \nPRODUCT PRICE: %.2f\n", p.name, p.price);
-	//printf("-------------\n");
+	printf("PRODUCT NAME: %s - PRODUCT PRICE: %.2f\n", p.name, p.price);
+
 }
 
 void printCustomer(struct Customer c)
 /*
-	Method to print customer details and shopping list 
+	Function to print customer details and shopping list 
 	Parameters:
-		c : struct : Holds information about customer name, budget and shopping list 
+		c : struct Customer : Holds information about customer name, budget and shopping list 
 */
 	
 {	
 	double total_price = 0.0;
+
 	// print customer name and budget
 	printf("CUSTOMER NAME: %s \nCUSTOMER BUDGET: %.2f\n", c.name, c.budget);
-	printf("-------------\n");
+	printf("--------------------------\n");
 
 	// iterates over customer shopping list 
 	for(int i = 0; i < c.index; i++) // 
@@ -68,21 +78,21 @@ void printCustomer(struct Customer c)
 		printf("%s ORDERS %d OF ABOVE PRODUCT\n", c.name, c.shoppingList[i].quantity); // prints information about customer name and quantity
 		double cost = c.shoppingList[i].quantity * c.shoppingList[i].product.price; // calculate cost
 		printf("The cost to %s will be e%.2f\n", c.name, cost); // print costs
-		printf("---------------\n");
-		total_price += cost;
+		printf("--------------------------\n");
+		total_price += cost; // increment total price variable
 	}
 
 	printf("%s's Provisional total cost: %.2f\n", c.name, total_price);
 	printf("%s's Provisional updated budget: %.2f\n", c.name, (c.budget - total_price));
-	printf("#######################\n");
+	printf("--------------------------\n");
 }
 
 struct Shop createAndStockShop()
 /*
-	Method to create Shop, read stock from CSV file 
+	Function to create Shop, read stock from CSV file 
 
 	Returns:
-		shop : struct 
+		shop : struct Shop
 */
 {
     FILE * fp; 
@@ -101,10 +111,9 @@ struct Shop createAndStockShop()
 	// convert string to float
 	float cash = atof(line);
 	
-	// create shop and initiate cash variable
+	// create struct Shop and assign cash variable
 	struct Shop shop = { cash };
-
-	// loop 
+	
     while ((read = getline(&line, &len, fp)) != -1) {
         
 		// method strtok breaks string str into a tokens using delimiter "," as string
@@ -112,7 +121,7 @@ struct Shop createAndStockShop()
 		char *p = strtok(NULL, ",");
 		char *q = strtok(NULL, ",");
 
-		// convert variables from string 
+		// convert variables from string to integer and double
 		int quantity = atoi(q);
 		double price = atof(p);
 
@@ -129,64 +138,104 @@ struct Shop createAndStockShop()
 
 		// increase shop stock index
 		shop.stock[shop.index++] = stockItem;
-		
     }
-	
 	return shop;
 }
+struct Product getProduct(struct Shop s, char* pname)
+/*
+	Function to check the shop for a product and return it
+	Parameters:
+		s : struct Shop : datatype struct contain shop stock and cash
+		pname: char : searched item in shop stock
+	
+	Return:
+		p : struct Product : datatype struct contain product name and price
 
-struct Product getProduct(struct Shop s, char* pname){
-   // check the shop for a product and return it
-   struct Product p;
-   for (int i = 0; i < s.index; i++){
-      if(strcmp(s.stock[i].product.name,pname)==0){
-        p = s.stock[i].product;
-      }
-   }
-   return p;
+*/
+{
+    struct Product p;
+
+	// loop over a shop index and compare related to index product name to searched phrase
+    for (int i = 0; i < s.index; i++){
+		// if strcmp returns 0 product was found and product p is returned
+    	if(strcmp(s.stock[i].product.name,pname)==0){
+    		p = s.stock[i].product;
+    	}
+    }
+	return p;
 };
 
 
 void clearScreen()
 /*
-https://stackoverflow.com/questions/2347770/how-do-you-clear-the-console-screen-in-c
-Method to Clear screen that will work for linux, windows and Mac OS. 
+	
+	Function to clear screen that will work for linux, windows and Mac OS. 
+	https://stackoverflow.com/questions/2347770/how-do-you-clear-the-console-screen-in-c
 */
 {
   system("cls||clear");
 }
 
 void printShop(struct Shop s)
+/*
+	Function to print in console shop cash, stock product and stock quantity
+
+	Parameters:
+		s : struct Shop : datatype struct contain shop stock and cash
+*/
 {
+	// print shop cash
 	printf("Shop has %.2f in cash\n", s.cash);
-	printf("----------------\n");
+	printf("--------------------------\n");
+	// for loop that print out shop product and product quantity
 	for (int i = 0; i < s.index; i++)
 	{
 		printProduct(s.stock[i].product);
 		printf("The shop has %d of the above\n", s.stock[i].quantity);
-		printf("------------------\n");
+		printf("--------------------------\n");
 	}
 }
 
 struct Customer CustOrder(struct Shop s, char* order_csv_file)
+/*
+	Function to read customer order from csv file. 
+	The csv file contains the customer's name, budget and a list of products that the customer wants to buy in the store.
+	The task of this function is to read the data line by line and pass it to datatype struct Customer 
+
+	Parameters:
+		s : struct Shop : datatype struct contain shop stock and cash
+		order_csv_file : char : name of the CSV file
+
+	Returns:
+		customer : struct Customer : datatype struct contains Customer details name, budget and shopping list
+*/
+
 {	
 	FILE * fp;
 	char * line = NULL;
 	size_t len = 0;
 	size_t read;
 
+	// opens csv file 
 	fp = fopen(order_csv_file,"r");
 
 	getline(&line, &len, fp);
 
+	// method strtok breaks string str into a tokens using delimiter "," as string
 	char *cName = strtok(line,",");
 	char *cBudget = strtok(NULL,",");
 
+	// method malloc allocates the requested memory and returns a pointer to it
+	// in the following case, memory is alocated for 50 characters
 	char *custName = malloc(sizeof(char)*50);
+
+	// method to copythe string pointed by source n to the destination name
 	strcpy(custName,cName);
 
+	// convert variables from string to double
 	double custBudget = atof(cBudget);
 
+	// create struct Customer and assign customer name and customer budget
 	struct Customer customer = {custName, custBudget};
 
 	while ((read = getline(&line, &len, fp)) != -1)
@@ -208,25 +257,60 @@ struct Customer CustOrder(struct Shop s, char* order_csv_file)
 };
 
 
-void finishOrder(struct Shop* s, struct Customer* c)
-{
-	/*  
-		The task of this function is to check whether a given order can be completed and to update the shop parameters.
+char* getProductName(struct Shop* s, char *pname){
 
-		In for loop:
-			1. We compare whether a given item from the client's list is in the store's assortment.
-			2. We check whether we have a sufficient quantity of a given product
-			3. we check whether the client has a sufficient amount of cash in the budget.
+	for(int j = 0; j < s->index; j++){
+		if(strcmp(s->stock[j].product.name,pname)==0){
+			return s->stock[j].product.name;   
+         }         
+   }
 
-		If the above conditions are met, we execute the order:
-		 - we reduce shop quantity,
-		 - we add order value to shop cash 
-		 - we reduce the client's budget.
+	return "NULL";
+}
 
-		param : struct Shop 
-		param : struct Customer
-	*/
+int getQuantity(struct Shop* s, char* pname)
+/*
+	Function to get stock quantity for product name
+	Parameters:
+		s : struct Shop : Holds information about customer name, budget and shopping list 
+		pname : char : product name 
 	
+	Returns:
+		q : int : quantity, if product not in sock returns 0
+*/
+{
+   // loop over shop index, get product name for each index and compare it to searched product
+   // if product found in shop stock return its quantity
+   // else return 0 
+   for (int i = 0; i < s->index; i++){
+      if(strcmp(s->stock[i].product.name,pname)==0){
+        int q = s->stock[i].quantity;
+		return q;		
+      }
+   }
+	return 0;
+
+};
+
+
+void finishOrder(struct Shop* s, struct Customer* c)
+/*  
+	The task of this function is to check whether a given order can be completed and to update the shop parameters.
+
+	In for loop:
+		1. We compare whether a given item from the client's list is in the store's assortment.
+		2. We check whether we have a sufficient quantity of a given product
+		3. we check whether the client has a sufficient amount of cash in the budget.
+
+	If the above conditions are met, we execute the order:
+		- we reduce shop quantity,
+		- we add order value to shop cash 
+		- we reduce the client's budget.
+
+	param : struct Shop 
+	param : struct Customer
+*/
+{	
 	int some_var = 0;
 
 	// for loop over a customer shopping list
@@ -240,33 +324,32 @@ void finishOrder(struct Shop* s, struct Customer* c)
 
 		double product_price = c->shoppingList[i].product.price; // Declare variable to hold product price
 		double order_value = customer_quantity * product_price; // variable to store order value (quantity * item_price)
-		
-		// inner loop over a shop products 
-		for (int j = 0; j<s->index; j++)
-		{
-			
-			char *shop_item = malloc(sizeof(char) * 50);
-			int shop_quantity = s->stock[j].quantity; // Declare variable to hold shop quantity for each product
 
-			strcpy(shop_item, s->stock[j].product.name);	
-				
-			// compare two products by it's name and store result in variable 
-			// int result_product = product_compare( customer_item, shop_item ); 
-			 int result_product = strcmp( customer_item, shop_item ); 
+		// call function getProductName  to check if customer product is in shop stock, returned value assign to shop_product   
+		char* shop_product = getProductName(s,customer_item); 
 
-			// if result_product is equal 1 it means that product from sopping list was found in shop stock
-			
-			if (result_product == 0)
-			{
-				some_var = 1;
-				// if the difference between shop_quantity and customer_quantity is greater than zero, the order can be fulfilled
-				if (shop_quantity - customer_quantity >= 0)
-				{
-					// if the order value is lower than the client's budget, the order can be processed
+		// call function getQuantity and returned value assign to shop_quantity
+		int shop_quantity = getQuantity(s,shop_product); 
+
+		// check if customer product not in shop
+		if (shop_product == "NULL"){
+			printf("I am very sorry, but unfortunately we do not sell %s \n",customer_item);
+		} else {
+			if (shop_quantity - customer_quantity >= 0)
+				{		
+				 	// if the order value is lower than the client's budget, the order can be processed
 					if (order_value < c->budget)
 					{
-						// Update stock quantity
-						s->stock[j].quantity = shop_quantity - customer_quantity;
+						// Loop over shop prroducts and update product quantity
+						for (int j = 0; j<s->index; j++)
+						{
+							if (s->stock[j].product.name == shop_product)
+							{
+								// Reduce product quantity in shop stock
+								s->stock[j].quantity = shop_quantity - customer_quantity;
+							}
+						}
+						
 						// Update shop cash
 						s->cash = s->cash + order_value;
 						// Update customer budget
@@ -275,36 +358,144 @@ void finishOrder(struct Shop* s, struct Customer* c)
 					}
 					else
 					{
-						printf("Sorry, but you are short of e%.2f to buy %d of %s", (order_value - c->budget), customer_quantity, customer_item );
+						printf("I'm very sorry but you don't have enough money, you are short of %.2f euro to buy %d %s\n", (order_value - c->budget), customer_quantity, customer_item );
 					}
 				}
 				else
 				{
-					printf("Sorry, We have not enough stock of %s\n",customer_item );	
+					printf("I'm sorry but unfortunately we don't have enough %s to cover your order for %d\n",customer_item, customer_quantity );	
 				}
-			}
 		}
-
-		if (some_var == 0)
-		{
-			printf("Sorry, We dont have %s in our stock\n",customer_item );	
-		} 
-		
-			
 	}
-
-
-
 	printf("*******************\n");
 }	
 
 	
+struct Customer liveCustomer(struct Shop s)
+/*
+	Function to implement live shop. 
+	It allow to buy product directly from console by entering customer name, budget, product name and quantity. 
+	
+	Then customer is created and returned to caller
 
+	Parameters:
+		s : struct Shop : Holds information about customer name, budget and shopping list 
+
+	Returns:
+		live_customer : struct Customer : // datatype struct contains Customer details name, budget and shopping list
+*/
+{
+	// create live_customer variable
+	struct Customer live_customer; 
+
+	// read customer name from console 
+	printf("\nPLEASE ENTER YOUR NAME: ");
+	char *cust_name = malloc(sizeof(char) * 50);
+	scanf("%s", cust_name);
+
+	// assign variable to live_customer.name 
+	live_customer.name = cust_name;
+	fflush(stdin); // clear the output
+
+	// read customer budget from console 
+	printf("\nPLEASE ENTER YOUR BUDGET: ");
+	double cust_budget;
+	
+	// https://stackoverflow.com/questions/210590/why-does-scanf-need-lf-for-doubles-when-printf-is-okay-with-just-f
+	// I found on stack overflow how to read double from console 
+	scanf("%lf", &cust_budget);
+
+	// assign variable to live_customer.budget
+	live_customer.budget = cust_budget;
+	fflush(stdin);
+
+	printf("\n");
+
+	printf("Here is my stock list\n");
+	printf("--------------------------\n");
+
+
+	// loop over product stock to print product in console
+	for (int i = 0; i < s.index; i++){
+        struct Product p = s.stock[i].product;
+		printProduct(p);
+		printf("--------------------------\n");		
+      }
+   
+
+	printf("What Would you likte to buy? \n");
+	printf("\n");
+	printf("\n");
+
+	
+	char choice;
+	live_customer.index = 0; 
+
+	// While loop will keep asking for product name and quantity until "n" is pressed
+	while (strcmp(&choice, "n") != 0)
+	{
+		// get product name 
+		printf("ENTER PRODUCT NAME: ");
+		char* customer_item = malloc(sizeof(char)*50);
+		scanf("\n%[^\n]%*c", customer_item); // regular expression that reads product name without last "\n" character
+
+		printf("\n");
+		fflush(stdin);
+
+		// get product quantity 
+		printf("ENTER PRODUCT QUANTITY: ");
+		int customer_quantity;
+		 
+		scanf("%d", &customer_quantity);
+		printf("\n");
+
+
+		
+		struct Product live_product = {customer_item,getProduct(s,customer_item).price};
+		struct ProductStock live_listItem = {live_product, customer_quantity};
+
+		live_customer.shoppingList[live_customer.index] = live_listItem;	
+		live_customer.index++;
+
+		printf("WOULD YOU LIKE TO BUY ANOTHER PRODUCT? y/n: ");
+		fflush(stdin); 
+		scanf("%s", &choice);
+
+	}
+	
+	return live_customer;
+
+} 
 
 // --------------- TEST MODE SECTION ----------------// 
+void show_options()
+/* 
+	Function to clear screen and display Main menu in console
+
+*/
+{
+	clearScreen();
+	printf("##################################\n");
+	printf("#                                #\n");
+	printf("#       WELCOME TO MY SHOP       #\n");
+	printf("#                                #\n");
+	printf("##################################\n");
+	printf("\n");
+	printf("1. Print Shop Stock\n");
+	printf("2. Get order from Shopping List\n");
+	printf("3. Test Shop\n");
+	printf("4. Live Mode\n");
+	printf("9. Return to Menu\n");
+	printf("0. Exit\n");
+}
 
 
 void testMenu(struct Shop shop)
+/* 
+	Function to display shop menu in test mode in console
+	Parameter:
+		s : struct Shop : Holds information about customer name, budget and shopping list
+*/
 {
 	
 	printf("##################################\n");
@@ -362,33 +553,16 @@ void testMenu(struct Shop shop)
 
 // ---------- GET ORDER FROM SHOPPING LIST -------------//
 
-void show_options()
-
-{
-	clearScreen();
-	printf("##################################\n");
-	printf("#                                #\n");
-	printf("#       WELCOME TO MY SHOP       #\n");
-	printf("#                                #\n");
-	printf("##################################\n");
-	printf("\n");
-	printf("1. Print Shop Stock\n");
-	printf("2. Get order from Shopping List\n");
-	printf("3. Test Shop\n");
-	printf("4. Live Mode\n");
-	printf("9. Return to Menu\n");
-	printf("0. Exit\n");
-}
 
 void displayMenu(struct Shop shop)
 /* 
-	Method to print menu, that user can choose between options,
-	return : char : option choosed by user 
-	
+	Function to print menu, that user can choose between options
+
+	Parameter:
+		s : struct Shop : Holds information about customer name, budget and shopping list
 */
 {
-
-	show_options();
+	show_options(); // display menu
 
 	int choice = -1;
 	
@@ -409,6 +583,9 @@ void displayMenu(struct Shop shop)
 		{
 			printf("**** Get order from ORDER.CSV shopping list ****\n\n");
 			struct Customer customer = CustOrder(shop, "..\\order.csv");
+			printf("\n");
+			printf("---===<< YOUR ORDER CONFIRMAION >>===---\n \n");
+
 			printCustomer(customer);
 			finishOrder(&shop, &customer);
 			printf("\n");
@@ -417,11 +594,26 @@ void displayMenu(struct Shop shop)
 		else if (choice == 3)
 		{
 			clearScreen(); // Call clear screen function
-			testMenu(shop); // call Test Manu for testing  
+			testMenu(shop); // call Test Menu for testing  
 			
-		} else if (choice == 3)
+		} else if (choice == 4)
 		{
-			printf("Welcome in Live mode");
+			
+			printf("Hello!!, Welcome To my Live Shop.  \n");
+			printf("\n");
+			printf("Please Take a look around \n");
+			printf("\n");
+
+			struct Customer live_customer = liveCustomer(shop);
+			
+			printf("\n");
+			printf("---===<< YOUR ORDER CONFIRMAION >>===---\n \n");
+			
+			printCustomer(live_customer);
+
+			finishOrder(&shop, &live_customer);
+			
+
 		} else if (choice == 9)
 		{
 			show_options(); // call show options function
